05cc742365aeae00e4d9195039d0e6c03dbd768d
Chad Vavra <chadvavra@gmail.com>
2024-07-24 19:19:22 -0400

    Added a new React component `ValuePropositionsContent` that fetches and displays value propositions based on a provided idea.

--- app/value-propositions/ValuePropositionsContent.tsx
+++ app/value-propositions/ValuePropositionsContent.tsx
@@ -1,15 +1,9 @@
-// @ts-nocheck
 'use client';
-import React, { Suspense } from 'react';
-import ValuePropositionsContent from './ValuePropositionsContent';
 
-export default function ValuePropositionsPage() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <ValuePropositionsContent />
-    </Suspense>
-  );
-}
+import React, { useState, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+
+const ValuePropositionsContent: React.FC = () => {
   const searchParams = useSearchParams();
   const [idea, setIdea] = useState('');
   const [valuePropositions, setValuePropositions] = useState('');
@@ -75,3 +69,5 @@ export default function ValuePropositionsPage() {
     </div>
   );
 }
+
+export default ValuePropositionsContent;
