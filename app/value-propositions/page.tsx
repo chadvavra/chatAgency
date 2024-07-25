cf23c9744e096d2884b082475a25ad7e4fa66471
Chad Vavra <chadvavra@gmail.com>
2024-07-25 08:02:28 -0400

    Removed unnecessary import and simplified the ValuePropositionsPage component.

--- app/value-propositions/page.tsx
+++ app/value-propositions/page.tsx
@@ -1,8 +1,3 @@
-import ValuePropositionsContent from './ValuePropositionsContent';
-
-export default function ValuePropositionsPage() {
-  return <ValuePropositionsContent />;
-}
 import React, { Suspense } from 'react';
 import ValuePropositionsContent from './ValuePropositionsContent';
 
