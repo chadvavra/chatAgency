6bf7f857578c35d5909bc88a292d62e27df70817
Chad Vavra <chadvavra@gmail.com>
2024-07-31 20:40:26 -0400

    modified:   components/LeftNavigation.tsx  removed 404s

--- components/LeftNavigation.tsx
+++ components/LeftNavigation.tsx
@@ -10,25 +10,25 @@ const LeftNavigation: React.FC<LeftNavigationProps> = ({ ideaId }) => {
     <nav className="bg-gray-100 p-4 rounded-lg">
       <ul className="space-y-2">
         <li>
-          <Link href={`/saved-idea?id=${ideaId}`} className="text-blue-600 hover:underline">
-            Idea Details
+          <Link href="/dashboard" className="text-blue-600 hover:underline">
+            Back
           </Link>
         </li>
-        <li>
+        {/* <li>
           <Link href={`/personas?id=${ideaId}`} className="text-blue-600 hover:underline">
             Personas
           </Link>
-        </li>
-        <li>
+        </li> */}
+        {/* <li>
           <Link href={`/keywords?id=${ideaId}`} className="text-blue-600 hover:underline">
             Keywords
           </Link>
-        </li>
-        <li>
+        </li> */}
+        {/* <li>
           <Link href={`/color-palette?id=${ideaId}`} className="text-blue-600 hover:underline">
             Color Palette
           </Link>
-        </li>
+        </li> */}
         <li>
           <Link href={`/image-generation?id=${ideaId}`} className="text-blue-600 hover:underline">
             Generate Product Image
