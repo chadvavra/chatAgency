214416bdd9b382ebe19a8f414050c45a985d3206
Chad Vavra <chadvavra@gmail.com>
2024-08-04 12:34:24 -0400

    feat: Add "Start Over" button to ValuePropositionsContent

--- app/value-propositions/ValuePropositionsContent.tsx
+++ app/value-propositions/ValuePropositionsContent.tsx
@@ -178,7 +178,7 @@ const ValuePropositionsContent = () => {
                     onClick={handleDiscard}
                     className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
                   >
-                    Discard
+                    Start Over
                   </button>
                 </div>
               )}
