97d79b1221d333cbfeb872e81f9c6d267dfad4ee
Chad Vavra <chadvavra@gmail.com>
2024-08-05 15:38:46 -0400

    modified:   app/background-home.png 	new file:   app/images/home/idea.png 	new file:   app/images/home/product.png 	new file:   app/images/home/values.png 	modified:   app/page.tsx
    New images and features for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import { oswald, plex } from '../utils/fonts';
 import { cookies } from "next/headers";
 import Image from 'next/image';
 import BackgroundHome from "./background-home.png";
+import ideaImg from "./images/home/idea.png";
+import valuesImg from "./images/home/values.png";
+import productImg from "./images/home/product.png";
 import IdeaForm from "@/components/IdeaForm";
 import Link from "next/link";
 
@@ -26,9 +29,9 @@ export default async function Home() {
           height={1080}
           className="w-full"
         />
-        <div className="absolute inset-0 flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
-          <h1 className={`${plex.className} text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold mb-4 sm:mb-6 text-white text-center`}>Agency For Your Ideas</h1>
-          <p className={`${oswald.className} text-lg sm:text-xl md:text-2xl mb-4 sm:mb-6 text-white text-center max-w-3xl`}>
+        <div className="absolute inset-0 flex flex-col items-center my-10 px-4 sm:px-6 lg:px-8">
+          <h1 className={`{plex.className} text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold mb-4 sm:mb-6 text-white text-center`}>Agency For Your Ideas</h1>
+          <p className={`{oswald.className} text-lg sm:text-xl md:text-2xl mb-4 sm:mb-6 text-white text-center max-w-3xl`}>
             The fastest way to take your ideas from start, to funded.
           </p>
         </div>
@@ -36,22 +39,28 @@ export default async function Home() {
 
       <section className="w-full bg-white-100 py-16">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8 text-center">Our Features</h2>
+          <h2 className="text-3xl font-bold mb-8 ">Features</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             <div className="bg-white shadow-md rounded-lg p-6 hover:shadow-lg transition-shadow duration-200">
-              <h3 className="text-lg font-semibold mb-2">Generate New Idea</h3>
-              <p className="text-gray-600 mb-4">Create a fresh business idea using AI.</p>
-              <Link href="/idea" className="text-blue-500 hover:text-blue-700">Get Started →</Link>
+              <h3 className="text-lg font-semibold mb-2">Idea Validation</h3>
+              <p className="text-gray-600 mb-4">Use AI to create a detailed idea.</p>
+              <Image src={ideaImg}
+                alt="idea screen capture image" />
+              {/* <Link href="/idea" className="text-blue-500 hover:text-blue-700">Get Started →</Link> */}
             </div>
             <div className="bg-white shadow-md rounded-lg p-6 hover:shadow-lg transition-shadow duration-200">
-              <h3 className="text-lg font-semibold mb-2">Explore Competitors</h3>
-              <p className="text-gray-600 mb-4">Analyze your competition in the market.</p>
-              <Link href="/competitors" className="text-blue-500 hover:text-blue-700">Explore →</Link>
+              <h3 className="text-lg font-semibold mb-2">Value Propostions</h3>
+              <p className="text-gray-600 mb-4">Find out what makes your idea great!</p>
+              <Image src={valuesImg}
+          alt="values screen capture image" />
+              {/* <Link href="/competitors" className="text-blue-500 hover:text-blue-700">Explore →</Link> */}
             </div>
             <div className="bg-white shadow-md rounded-lg p-6 hover:shadow-lg transition-shadow duration-200">
-              <h3 className="text-lg font-semibold mb-2">Generate Images</h3>
-              <p className="text-gray-600 mb-4">Create visuals for your business idea.</p>
-              <Link href="/image-generation" className="text-blue-500 hover:text-blue-700">Create Images →</Link>
+              <h3 className="text-lg font-semibold mb-2">Generate Visuals</h3>
+              <p className="text-gray-600 mb-4">Create colors, moodboards, and product shots.</p>
+              <Image src={productImg}
+          alt="product example image" />
+              {/* <Link href="/image-generation" className="text-blue-500 hover:text-blue-700">Create Images →</Link> */}
             </div>
           </div>
         </div>
