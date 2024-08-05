1e35e4873e01f10053b592b1570ec0905f927e7f
Chad Vavra <chadvavra@gmail.com>
2024-08-05 18:39:41 -0400

    modified:   app/about/page.tsx 	modified:   components/Footer.tsx  a new about page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,6 +5,9 @@ const Footer = () => {
     <footer className="bg-gray-100 mt-8">
       <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
         <nav className="flex justify-center space-x-4">
+        <Link href="/about" className="text-gray-500 hover:text-gray-900">
+            About
+          </Link>
           <Link href="/terms" className="text-gray-500 hover:text-gray-900">
             Terms
           </Link>
