f3cad12a106529bc12412b3c1e30a83232e9c6b0
Chad Vavra <chadvavra@gmail.com>
2024-08-06 09:58:45 -0400

    modified:   app/about/page.tsx 	modified:   utils/fonts.js

--- utils/fonts.js
+++ utils/fonts.js
@@ -1,3 +1,3 @@
 import { IBM_Plex_Sans, Oswald } from 'next/font/google';
-export const oswald = Oswlad({ subsets: ['latin'] });
-export const plex = IBM_Plex_Sans({ subsets: ['latin'] })
+export const oswald = Oswald({ subsets: ['latin'], weight: ['600', '300'], });
+export const plex = IBM_Plex_Sans({ subsets: ['latin'], weight: ['600', '300'], })
